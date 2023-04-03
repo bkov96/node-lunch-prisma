d48c73c37d30d81a73e69b9487e5791aecb27daf
bence.kovacs2 <bence.kovacs2@supercharge.io>
2023-04-03 20:17:46 +0200

    chore: extend createCategoryDto

--- src/api/categories/dto/create-category.dto.ts
+++ src/api/categories/dto/create-category.dto.ts
@@ -1,7 +1,10 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { Length } from 'class-validator';
+import { Prisma } from '@prisma/client';
 
-export class CreateCategoryDto {
+export class CreateCategoryDto
+  implements Pick<Prisma.CategoryCreateInput, 'name'>
+{
   @ApiProperty({ example: 'fantasy' })
   @Length(1, 30)
   name: string;
