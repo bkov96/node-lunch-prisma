fb3279c9ddef0f17bc006635177ce949297f0627
bence.kovacs2 <bence.kovacs2@supercharge.io>
2024-01-16 15:34:01 +0100

    refactor: remove borrowings module

--- src/api/borrowings/borrowings.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { BorrowingsController } from './borrowings.controller';
-import { BorrowingsService } from './borrowings.service';
-
-@Module({
-  controllers: [BorrowingsController],
-  providers: [BorrowingsService],
-})
-export class BorrowingsModule {}
